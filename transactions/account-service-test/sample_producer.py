171253f7758859b4c15d577d88c211663e30014c
tom1299 <tom1299@jeetatwork.com>
2020-11-11 14:39:29 +0100

    Change to confluentic kafka and add transactions

--- transactions/account-service-test/sample_producer.py
+++ transactions/account-service-test/sample_producer.py
@@ -3,22 +3,27 @@ import uuid
 import json
 import time
 
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 
 bootstrap_servers = '172.18.0.2:31147'
 topic_name = 'account-events'
 
+producer = Producer({
+    'bootstrap.servers': bootstrap_servers,
+    'transactional.id': "sample_producer"
+})
 
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+producer.init_transactions()
 
 for i in range(10):
     accountEventId = uuid.uuid4();
     transactionId = uuid.uuid4();
     accountNumber = str(random.choice(list(range(1000, 1003))))
+    amount = random.choice(list(range(1, 100)))
     order = {
       "id": f"{accountEventId}",
       "transactionId": f"{transactionId}",
-      "amount": random.choice(list(range(1, 100))),
+      "amount": amount,
       "userId": f"{accountNumber}",
       "status": "new",
       "accountNumber": f"{accountNumber}",
@@ -26,8 +31,15 @@ for i in range(10):
     }
 
     message = json.dumps(order);
-    print(f'Sending {message}')
+    print(f'Sending account event {message}')
 
-    future = producer.send(topic_name, f'{message}'.encode('utf-8'), accountNumber.encode("utf-8"))
-    result = future.get(timeout=60)
+    producer.begin_transaction()
+    producer.produce(topic_name, f'{message}'.encode('utf-8'), accountNumber.encode("utf-8"))
+
+    if (amount % 2) == 0:
+        print(f'Aborting transaction for account event {accountEventId}')
+        producer.abort_transaction()
+    else:
+        producer.commit_transaction()
+
     time.sleep(0.5)
