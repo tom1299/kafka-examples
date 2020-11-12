7868bd1a9aabcad79fc8614d7cd51daa8c3ab03d
tom1299 <tom1299@jeetatwork.com>
2020-11-12 07:24:42 +0100

    Smaller refactorings

--- transactions/account-service-test/sample_producer.py
+++ transactions/account-service-test/sample_producer.py
@@ -6,7 +6,6 @@ import time
 from confluent_kafka import Producer
 
 bootstrap_servers = '172.18.0.2:31147'
-topic_name = 'account-events'
 
 producer = Producer({
     'bootstrap.servers': bootstrap_servers,
@@ -20,7 +19,7 @@ for i in range(10):
     transactionId = uuid.uuid4();
     accountNumber = str(random.choice(list(range(1000, 1003))))
     amount = random.choice(list(range(1, 100)))
-    order = {
+    account_event = {
       "id": f"{accountEventId}",
       "transactionId": f"{transactionId}",
       "amount": amount,
@@ -30,11 +29,12 @@ for i in range(10):
       "type": random.choice(["deposit", "withdraw"])
     }
 
-    message = json.dumps(order);
+    message = json.dumps(account_event);
     print(f'Sending account event {message}')
 
     producer.begin_transaction()
-    producer.produce(topic_name, f'{message}'.encode('utf-8'), accountNumber.encode("utf-8"))
+    producer.produce('account-events', f'{message}'.encode('utf-8'), accountNumber.encode("utf-8"))
+    # producer.produce('order-events', f'{message}'.encode('utf-8'), accountNumber.encode("utf-8"))
     producer.flush()
 
     if (amount % 2) == 0:
--- transactions/account-service/src/test/java/com/tom/kafka/examples/account/service/SimpleConsumerReadingUncommitted.java
+++ transactions/account-service/src/test/java/com/tom/kafka/examples/account/service/SimpleConsumerReadingUncommitted.java
@@ -1,36 +1,55 @@
 package com.tom.kafka.examples.account.service;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.IsolationLevel;
+import org.apache.kafka.common.metrics.stats.Count;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.concurrent.*;
 
 public class SimpleConsumerReadingUncommitted {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Properties props = createDefaultProperties();
+        KafkaConsumer<String, String> readUncommittedConsumer =
+                createConsumer(props, "read_uncommitted", "account-events");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        Runnable runnableTask = () -> {
+            while (true) {
+                ConsumerRecords<String, String> records = readUncommittedConsumer.poll(Duration.ofSeconds(100));
+                for (ConsumerRecord<String, String> record : records)
+                    System.out.println(record.offset() + ": " + record.value());
+            }
+        };
+
+        executorService.submit(runnableTask);
+        CountDownLatch latch = new CountDownLatch(2);
+        latch.await();
+    }
+
+    private static KafkaConsumer<String, String> createConsumer(Properties props, String isolationLevel, String topic) {
+        Properties uncommittedProperties = (Properties) props.clone();
+        uncommittedProperties.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, isolationLevel);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(uncommittedProperties);
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+
+    private static Properties createDefaultProperties() {
         Properties props = new Properties();
         props.put("bootstrap.servers", "172.18.0.2:31147");
         props.put("group.id", "consumer-tutorial");
         props.put("key.deserializer", StringDeserializer.class.getName());
         props.put("value.deserializer", StringDeserializer.class.getName());
         props.put("enable.auto.commit", false);
-//        props.put("enable.partition.eof", true);
-//        props.put("auto.offset.reset", "earliest");
-        props.put("isolation.level", "read_uncommitted");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("account-events"));
-
-        try {
-            while (true) {
-                ConsumerRecords<String, String> records = consumer.poll(1000);
-                for (ConsumerRecord<String, String> record : records)
-                    System.out.println(record.offset() + ": " + record.value());
-            }
-        } finally {
-            consumer.close();
-        }
+        return props;
     }
 }
