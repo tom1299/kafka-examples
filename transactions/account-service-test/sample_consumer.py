5f66331ef4e63073db868619a7cf9c7b3d368322
tom1299 <tom1299@jeetatwork.com>
2020-11-16 11:53:43 +0100

    Add sample consumer

---
+++ transactions/account-service-test/sample_consumer.py
@@ -0,0 +1,24 @@
+from confluent_kafka import Consumer
+
+bootstrap_servers = '172.18.0.2:30195'
+
+c = Consumer({
+    'bootstrap.servers': bootstrap_servers,
+    'group.id': 'account-events-consumers',
+    'auto.offset.reset': 'earliest'
+})
+
+c.subscribe(['processed-account-events'])
+
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+
+    print(f"Received message: {msg.value().decode('utf-8')}, from topic {msg.topic()}")
+
+c.close()
